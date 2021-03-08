146667d1b1853d7fa8ce2ef149782bf5d97a812e
viksya <77070561+viksya@users.noreply.github.com>
2021-03-08 18:45:14 +0200

    Add project files.

---
+++ C2_Day15/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C2_Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
