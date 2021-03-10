719c789ddb846a4ba9ecad354704fd2e7fff3353
viksya <77070561+viksya@users.noreply.github.com>
2021-03-10 19:06:36 +0200

    MoviesController added

--- CinemaWeb/Controllers/HomeController.cs
+++ CinemaWeb/Controllers/HomeController.cs
@@ -12,22 +12,13 @@ namespace CinemaWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private CategoryManager category = new CategoryManager();
+       
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Category()
-        {
-            var data = category.GetCategories();
-            return View(data);
-        }
-
-        public IActionResult Booking()
-        {
-            return View();
-        }
+        
     }
 }
---
+++ CinemaWeb/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using CinemaLogic.Managers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaWeb.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private CategoryManager category = new CategoryManager();
+        public IActionResult Category()
+        {
+            var data = category.GetCategories();
+            return View(data);
+        }
+
+        public IActionResult Booking()
+        {
+            return View();
+        }
+    }
+}
